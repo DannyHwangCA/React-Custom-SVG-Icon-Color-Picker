4f0110f220cac99c2828430da482947627b77736
Danny Hwang <dannyhwangca@gmail.com>
2020-04-03 23:08:10 -0700

    added color input & input css

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 (function() {
     "use strict";
     
-function Icon(props) {
-    return (
-    <i class='fas fa-heartbeat'></i>
-    ) 
-}
-
-function IconCustomizer(props) {
-    return (
-    <div className="outer-icon-container">
-        <div className="inner-icon-container">
-        <Icon />
+    function Icon(props) {
+      return (
+        <i class='custom-icon fas fa-heartbeat'></i>
+      );
+    }
+    
+    function ColorWell(props) {
+      return (
+        <div className="color-input-container">
+          <input class="color-input" type="color" value="#ffadff" />
         </div>
-    </div>
-    )
-}
-
-ReactDOM.render(<IconCustomizer />, document.getElementById('app'));
+      );
+    }
+    
+    function IconCustomizer(props) {
+      return (
+        <div className="outer-icon-container">
+          <div className="inner-icon-container">
+            <Icon />
+            <ColorWell />
+          </div>
+        </div>
+      );
+    }
+    
+    ReactDOM.render(<IconCustomizer />, document.getElementById('app'));
 })();
   
