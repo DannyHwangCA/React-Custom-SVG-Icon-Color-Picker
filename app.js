b0c3b21b38fbaf0de3e1c6600300c44e6c73ce74
Danny Hwang <dannyhwangca@gmail.com>
2020-04-03 20:19:38 -0700

    Stater React Test

---
+++ app.js
@@ -0,0 +1,11 @@
+(function() {
+    "use strict";
+    
+    var customizer = React.createElement(
+      'div', 
+      { className: "customizer" },
+      "Test"
+    )
+    
+    ReactDOM.render(customizer, document.getElementById('app'));
+})();
